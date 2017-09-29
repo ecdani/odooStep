8a12482e9e002f294a194361c0d89bbd976fd9fc
ecdani <ec.dani@gmail.com>
2017-09-29 11:42:52 +0200

    Conexión con Odoo desde el plugin exitosa

--- odooStep/stepodooStepApplication.php
+++ odooStep/stepodooStepApplication.php
@@ -2,6 +2,7 @@
 /**
  * Este archivo es el callback del step, aquí se procesa el paso en el case (instancia de proceso )
  */
+ require_once('/opt/plugins/odooStep/odooStep/dependencies/ripcord/ripcord.php');
 try {
   global $Fields;
   $oHeadPublisher = &headPublisher::getSingleton();
@@ -27,10 +28,18 @@ try {
   $username = "ec.dani@gmail.com";
   $password = "a";
 
-  $common = ripcord::client("$url/xmlrpc/2/common");
+  $common = ripcord::client("$url/xmlrpc/2/common"); /*Fatal error: Class 'ripcord' not found in /opt/plugins/odooStep/odooStep/stepodooStepApplication.php on line 30*/
   print_r ($common->version());
   echo "Hello world!<br>";
+   /*﻿Array ( 
+    [server_serie] => 10.0 
+    [server_version_info] => Array ( [0] => 10 [1] => 0 [2] => 0 [3] => final [4] => 0 [5] => ) 
+    [server_version] => 10.0 
+    [protocol_version] => 1 
+    ) Hello world!*/
 
+/*Notice: Undefined variable: http_response_header in /opt/plugins/odooStep/odooStep/dependencies/ripcord/ripcord_client.php on line 485
+Could not access http://localhost:8069/xmlrpc/2/common*/
 
   $uid = $common->authenticate($db, $username, $password, array());
   //print_r ($uid);
@@ -39,12 +48,13 @@ try {
   $output = $models->execute_kw($db, $uid, $password,
       'res.partner', 'check_access_rights',
       array('read'), array('raise_exception' => false));
-  print_r ($output);
+  print_r ($output);/*Output: 1*/
+
   $output = $models->execute_kw($db, $uid, $password,
       'res.partner', 'search', array(
           array(array('is_company', '=', true),
                 array('customer', '=', true))));
-  print_r ($output);
+  print_r ($output); /*Output: Array ( [0] => 8 [1] => 12 [2] => 9 [3] => 45 [4] => 11 [5] => 13 ) */
 
   
   G::RenderPage("publish", "extJs");
